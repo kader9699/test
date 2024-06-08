bf950925916dcaaf76b2ce45a5b3e0f5079df668
kader9699 <kaderaboubacarslm@gmail.com>
2024-06-08 23:04:07 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from response import response_generator
+st.title("This is my chatbot App")
+
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        response = st.write_stream(response_generator())
+        # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
